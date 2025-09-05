a885bf552375315d03fce98e48558518bf69ef24
Shubham kumar <shubh@Shubhams-MacBook-Air.local>
2025-09-05 18:54:38 +0530

    Fixed bug in detail button

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -73,6 +73,12 @@ const PropertyCard = ({ property }) => {
                 {" "}
                 {property.location.city} {property.location.state}{" "}
               </span>
+              <span
+                href={`/properties/${property._id}`}
+                className=" ml-18 h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+              >
+                Details
+              </span>
             </div>
           </div>
         </div>
